3f9c34990714b2dbb88ba1f5261bd36a8c5c6462
Stelios Giakoumidis <stelios.gia.sweden@gmail.com>
2019-11-17 11:38:44 +0100

    Insert project

---
+++ NotificationDbManager/Program.cs
@@ -0,0 +1,24 @@
+﻿using DbUp;
+using System;
+using System.Reflection;
+
+namespace NotificationDbManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Server=192.168.0.29,1433;Database=master;User Id=SA;Password=Stelios!!!!1111;";
+            EnsureDatabase.For.SqlDatabase(connectionString); //Creates database if not 
+            var upgradeEngineBuilder = DeployChanges.To
+                            .SqlDatabase(connectionString)
+                            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                            .WithTransaction()
+                            .LogToConsole();
+
+            var upgrader = upgradeEngineBuilder.Build();
+            if (upgrader.IsUpgradeRequired()) upgrader.PerformUpgrade();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NotificationDbManager/Scripts/Scripts001 - NotificationServiceDb.sql
@@ -0,0 +1,79 @@
+﻿CREATE DATABASE NotificationService
+GO
+
+USE [NotificationService]
+GO
+/****** Object:  Table [dbo].[RegularSendout]    Script Date: 2019-06-03 21:12:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RegularSendout](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ReminderName] [nvarchar](max) NOT NULL,
+	[StartDate] [datetime] NOT NULL,
+	[RepetitionFrequency] [nvarchar](30) NOT NULL,
+	[ExecutionTime] [int] NOT NULL,
+	[DayOfTheWeek] [nvarchar](10) NOT NULL,
+	[LastRunAt] [datetime] NULL,
+	[Parameters] [nvarchar](max) NULL,
+	[Username] [nvarchar](max) NULL,
+	[UserGroup] [nvarchar](max) NULL,
+	[TemplateName] [nvarchar](max) NULL,
+ CONSTRAINT [PK_RegSendout] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Templates]    Script Date: 2019-06-03 21:12:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Templates](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[NotificationText] [nvarchar](max) NOT NULL,
+	[NotificationName] [nvarchar](max) NOT NULL,
+	[NotificationPriority] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Templates] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[UserGroups]    Script Date: 2019-06-03 21:12:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserGroups](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[GroupName] [nvarchar](max) NOT NULL,
+	[UserIds] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 2019-06-03 21:12:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](max) NOT NULL,
+	[Email] [nvarchar](max) NOT NULL,
+	[Sms] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
